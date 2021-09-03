ff7b4448a7b0d23b43cd292474ac1bec3682dfc1
hd4y2t <hd4y2t@gmail.com>
2021-09-03 23:04:21 +0700

    random number

--- b.go
+++ b.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	var names = []string{"John", "Wick"}
-	printMessage("hallo", names)
+	// 	var names = []string{"John", "Wick"}
+	// 	printMessage("hallo", names)
+	// }
+
+	// func printMessage(message string, arr []string) {
+	// 	var nameString = strings.Join(arr, " ")
+	// 	fmt.Println(message, nameString)
+
+	rand.Seed(time.Now().Unix())
+
+	var randomValue int
+
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number : ", randomValue)
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number : ", randomValue)
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number : ", randomValue)
 }
 
-func printMessage(message string, arr []string) {
-	var nameString = strings.Join(arr, " ")
-	fmt.Println(message, nameString)
+func randomWithRange(min, max int) int {
+	var value = rand.Int()%(max-min+1) + min
+	return value
 }
